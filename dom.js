176579f89e2361e39887d0fbc038cb6b1992f667
Sugyan98 <sugyanmahakud@gmail.com>
2023-03-23 18:11:19 +0530

    adding to existing repo

---
+++ dom.js
@@ -0,0 +1,45 @@
+// Examine the Document Object
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTSBYID
+
+// console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+headerTitle.style.borderBottom = 'solid 3px #000';
+header.style.borderBottom = 'solid 3px #000';
+document.getElementById('title').style.fontWeight = 'bold';
+
+// GETELEMENTSBYCLASSNAME
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
