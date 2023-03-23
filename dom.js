c0037f05b36c1a0f801a8e834f57bdc11ca1dd9b
Sugyan98 <sugyanmahakud@gmail.com>
2023-03-24 01:56:57 +0530

    GetElementsByTagName

--- dom.js
+++ dom.js
@@ -30,14 +30,26 @@ header.style.borderBottom = 'solid 3px #000';
 document.getElementById('title').style.fontWeight = 'bold';
 
 // GETELEMENTSBYCLASSNAME
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-items[2].style.backgroundColor = 'green';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+// items[2].style.backgroundColor = 'green';
 
-for (var i = 0; i < items.length; i++){
-    items[i].style.fontWeight = 'bold';
+// for (var i = 0; i < items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+li[2].style.backgroundColor = 'green';
+
+for (var i = 0; i < li.length; i++){
+    li[i].style.fontWeight = 'bold';
 }
