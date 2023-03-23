679917178ff11ddc559a6bbf8962df10569dd75a
Sugyan98 <sugyanmahakud@gmail.com>
2023-03-24 01:27:06 +0530

    GetElementsByClassName

--- dom.js
+++ dom.js
@@ -30,16 +30,14 @@ header.style.borderBottom = 'solid 3px #000';
 document.getElementById('title').style.fontWeight = 'bold';
 
 // GETELEMENTSBYCLASSNAME
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundColor = 'yellow';
+items[2].style.backgroundColor = 'green';
+
+for (var i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
